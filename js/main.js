1095927595291970d8ddd8137fdef75d49e39510
ductaithuhai <nductai94@gmail.com>
2023-10-29 23:13:58 +0700

    update a little

--- js/main.js
+++ js/main.js
@@ -7,62 +7,72 @@ function getComNumber(min, max) {
 
 function getComputerChoice(comNumber){
     if (comNumber==1){
-          return 'kéo';
+          return 'scissor';
         }
 
     if (comNumber==2){
-          return 'búa';
+          return 'rock';
         }
 
     if (comNumber==3){
-          return 'bao';
+          return 'paper';
         }
 }
 
 function playRound(a,b){
-  if((a=='kéo'&& b=='kéo')||
-    (a=='búa'&& b=='búa')||
-     (a=='bao'&& b=='bao'))  {
+  if((a=='scissor'&& b=='scissor')||
+    (a=='rock'&& b=='rock')||
+     (a=='paper'&& b=='paper'))  {
     return 'Hai bạn đã hòa nhau hiệp này';
 }
 
-if((a=='kéo'&& b=='búa')||
-    (a=='búa'&& b=='bao')||
-      (a=='bao'&& b=='kéo')) 
+if((a=='scissor'&& b=='rock')||
+    (a=='rock'&& b=='paper')||
+      (a=='paper'&& b=='scissor')) 
        {
         clientScore++;
         return 'Bạn đã thắng hiệp này! '+  b +' đã đánh bại ' + a;
         }
 
-if((a=='kéo'&& b=='bao')||
-    (a=='búa'&& b=='kéo')||
-      (a=='bao'&& b=='búa')) 
+if((a=='scissor'&& b=='paper')||
+    (a=='rock'&& b=='scissor')||
+      (a=='paper'&& b=='rock')) 
         {
           comScore++;
           return 'Bạn đã thua hiệp này! '+  a +' đã đánh bại ' + b;
 }
 }
+let clientChoice;
+clientChoice= document.getElementsByClassName("rock").onclick = function() {youchooserock()};
+function youchooserock(){
+  return clientChoice='rock';
+}
 
-function game(soluot){
-    for(let i=1;i<=soluot;i++){
-      let comNumber=getComNumber(1,3);
-      let clientChoice=prompt("Vui lòng đưa ra lựa chọn của bạn: ").toLowerCase();
-      let comChoice=getComputerChoice(comNumber);
-      let message= playRound(comChoice,clientChoice);
-      console.log(clientChoice);
-      console.log(comChoice);
-      console.log(message);
-    }
-    }
-    game(5);
-    let ketqua;
-    if(clientScore>comScore){
-      ketqua='Bạn đã chiến thắng chung cuộc!!!!!!!!';
-    } else if(clientScore==comScore){
-      ketqua='Hai bạn đã hòa nhau.';
-    } else {
-      ketqua='Bạn đã thất bại hoàn toàn :)))))))';
-    }
-        console.log(ketqua);
-        console.log('Điểm của bạn: ',clientScore);
-        console.log('Điểm của máy: ',comScore);
+document.getElementsByClassName("paper").onclick = function() {youchoosepaper()};
+function youchooserock(){
+  return clientChoice='paper';
+}
+
+document.getElementsByClassName("scis").onclick = function() {youchoosescis()};
+function youchooserock(){
+  return clientChoice='scissor';
+}
+console.log(clientChoice);
+      // let comNumber=getComNumber(1,3);
+      // let comChoice=getComputerChoice(comNumber);
+      // let message= playRound(comChoice,clientChoice);
+      // console.log(clientChoice);
+      // console.log(comChoice);
+      // console.log(message);
+    // game(5);
+    // let ketqua;
+    // if(clientScore>comScore){
+    //   ketqua='Bạn đã chiến thắng chung cuộc!!!!!!!!';
+    // } else if(clientScore==comScore){
+    //   ketqua='Hai bạn đã hòa nhau.';
+    // } else {
+    //   ketqua='Bạn đã thất bại hoàn toàn :)))))))';
+    // }
+    //     console.log(ketqua);
+    //     console.log('Điểm của bạn: ',clientScore);
+    //     console.log('Điểm của máy: ',comScore);
