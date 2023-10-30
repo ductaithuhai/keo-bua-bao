4dfbfd84fefd4516b2ab710f628ae8904d46df7c
ductaithuhai <nductai94@gmail.com>
2023-10-30 23:17:08 +0700

    finish ui

--- js/main.js
+++ js/main.js
@@ -8,108 +8,132 @@ function getComNumber(min, max) {
 
 function getComputerChoice(comNumber){
     if (comNumber==1){
-          return 'scissor';
+          return 'Scissor';
         }
 
     if (comNumber==2){
-          return 'rock';
+          return 'Rock';
         }
 
     if (comNumber==3){
-          return 'paper';
+          return 'Paper';
         }
 }
 
 function playRound(a,b){
-  if((a=='scissor'&& b=='scissor')||
-    (a=='rock'&& b=='rock')||
-     (a=='paper'&& b=='paper'))  {
-    return 'Hai bạn đã hòa nhau hiệp này';
+  if((a=='Scissor'&& b=='Scissor')||
+    (a=='Rock'&& b=='Rock')||
+     (a=='Paper'&& b=='Paper'))  {
+    return 'It\'s a Tie';
 }
 
-if((a=='scissor'&& b=='rock')||
-    (a=='rock'&& b=='paper')||
-      (a=='paper'&& b=='scissor')) 
+if((a=='Scissor'&& b=='Rock')||
+    (a=='Rock'&& b=='Paper')||
+      (a=='Paper'&& b=='Scissor')) 
        {
         clientScore++;
-        return 'Bạn đã thắng hiệp này! '+  b +' đã đánh bại ' + a;
+        return 'You win this round!! '+  b +' beated ' + a;
         }
 
-if((a=='scissor'&& b=='paper')||
-    (a=='rock'&& b=='scissor')||
-      (a=='paper'&& b=='rock')) 
+if((a=='Scissor'&& b=='Paper')||
+    (a=='Rock'&& b=='Scissor')||
+      (a=='Paper'&& b=='Rock')) 
         {
           comScore++;
-          return 'Bạn đã thua hiệp này! '+  a +' đã đánh bại ' + b;
+          return 'You loose this round! '+  a +' beated ' + b;
 }
 }
+
+let endGameBackground = document.getElementsByClassName("endGameBackground");
 
 function ketqua(clScore,cScore){
   if(clScore==5&&cScore<5){
     comScore=0;
     clientScore=0;
-    return 'Human Destroy Orc!! You Happy Ever After';
+    endGameBackground[0].style.display='block';
+    return 'Human Destroy Orc!! Happy Ever After';
   }
   if(comScore==5&&clientScore<5){
     comScore=0;
     clientScore=0;
-    return'Orc Destroy Human!! Let Begin The Age Of Extinction';
+    endGameBackground[0].style.display='block';
+    return'Orc Destroy Human!! Age Of Extinction';
   }
   }
 
 const yourScore = document.getElementsByClassName("yourScore");
 const orcScore = document.getElementsByClassName("orcScore");
-let endGameMess = document.getElementsByClassName("endGameMess");
+let yourSym=document.getElementsByClassName("yourSym");
+let orcSym=document.getElementsByClassName("orcSym");
+let endGameMes=document.getElementsByClassName("endGameMes");
+let roundMes=document.getElementsByClassName("roundMes");
 
 let clientChoice;
 let rock = document.getElementsByClassName("rock");
 rock[0].addEventListener("click", function() {
-  clientChoice='rock';
+  clientChoice='Rock';
   let comNumber=getComNumber(1,3);
   let comChoice=getComputerChoice(comNumber);
   let message= playRound(comChoice,clientChoice);
   yourScore[0].innerHTML=clientScore;
   orcScore[0].innerHTML=comScore;
   ketqua1= ketqua(clientScore,comScore);
-  endGameMess[0].innerHTML=ketqua1;
-  console.log(clientChoice);
-  console.log(comChoice);
-  console.log(message);
-  console.log('Điểm của bạn: ',clientScore);
-  console.log('Điểm của máy: ',comScore);
+  endGameMes[0].innerHTML=ketqua1;
+  yourSym[0].src='../assets/img/rock.png';
+  if(comChoice=='Rock'){
+    orcSym[0].src='../assets/img/rock.png';
+  } else if(comChoice=='Paper'){
+    orcSym[0].src='../assets/img/paper.png';
+  } else{
+    orcSym[0].src='../assets/img/scis.png';
+  }
+  roundMes[0].innerHTML=message;
 });
 
 let paper = document.getElementsByClassName("paper");
 paper[0].addEventListener("click", function() {
-  clientChoice='paper';
+  clientChoice='Paper';
   let comNumber=getComNumber(1,3);
   let comChoice=getComputerChoice(comNumber);
   let message= playRound(comChoice,clientChoice);
   ketqua1= ketqua(clientScore,comScore);
   yourScore[0].innerHTML=clientScore;
   orcScore[0].innerHTML=comScore;
-  endGameMess[0].innerHTML=ketqua1;
-  console.log('Điểm của bạn: ',clientScore);
-  console.log('Điểm của máy: ',comScore);
-  console.log(clientChoice);
-  console.log(comChoice);
-  console.log(message);
+  endGameMes[0].innerHTML=ketqua1;
+  yourSym[0].src='../assets/img/paper.png';
+  if(comChoice=='Rock'){
+    orcSym[0].src='../assets/img/rock.png';
+  } else if(comChoice=='Paper'){
+    orcSym[0].src='../assets/img/paper.png';
+  } else{
+    orcSym[0].src='../assets/img/scis.png';
+  }
+  roundMes[0].innerHTML=message;
 });
 
 let scis = document.getElementsByClassName("scis");
 scis[0].addEventListener("click", function() {
-  clientChoice='scissor';
+  clientChoice='Scissor';
   let comNumber=getComNumber(1,3);
   let comChoice=getComputerChoice(comNumber);
   let message= playRound(comChoice,clientChoice);
   ketqua1= ketqua(clientScore,comScore);
   yourScore[0].innerHTML=clientScore;
   orcScore[0].innerHTML=comScore;
-  endGameMess[0].innerHTML=ketqua1;
-  console.log('Điểm của bạn: ',clientScore);
-  console.log('Điểm của máy: ',comScore);
-  console.log(clientChoice);
-  console.log(comChoice);
-  console.log(message);
+  endGameMes[0].innerHTML=ketqua1;
+  yourSym[0].src='../assets/img/scis.png';
+  if(comChoice=='Rock'){
+    orcSym[0].src='../assets/img/rock.png';
+  } else if(comChoice=='Paper'){
+    orcSym[0].src='../assets/img/paper.png';
+  } else{
+    orcSym[0].src='../assets/img/scis.png';
+  }
+  roundMes[0].innerHTML=message;
 });
 
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  endGameBackground[0].style.display = "none";
+}
