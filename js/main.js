6c8fb8a9e5d708122d2f66b826069fa58b111684
ductaithuhai <nductai94@gmail.com>
2023-10-30 14:39:46 +0700

    update graphic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let clientScore=0;
 let comScore=0;
+let ketqua1;
 
 function getComNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -42,37 +43,73 @@ if((a=='scissor'&& b=='paper')||
           return 'Bạn đã thua hiệp này! '+  a +' đã đánh bại ' + b;
 }
 }
+
+function ketqua(clScore,cScore){
+  if(clScore==5&&cScore<5){
+    comScore=0;
+    clientScore=0;
+    return 'Human Destroy Orc!! You Happy Ever After';
+  }
+  if(comScore==5&&clientScore<5){
+    comScore=0;
+    clientScore=0;
+    return'Orc Destroy Human!! Let Begin The Age Of Extinction';
+  }
+  }
+
+const yourScore = document.getElementsByClassName("yourScore");
+const orcScore = document.getElementsByClassName("orcScore");
+let endGameMess = document.getElementsByClassName("endGameMess");
+
 let clientChoice;
-clientChoice= document.getElementsByClassName("rock").onclick = function() {youchooserock()};
-function youchooserock(){
-  return clientChoice='rock';
-}
+let rock = document.getElementsByClassName("rock");
+rock[0].addEventListener("click", function() {
+  clientChoice='rock';
+  let comNumber=getComNumber(1,3);
+  let comChoice=getComputerChoice(comNumber);
+  let message= playRound(comChoice,clientChoice);
+  yourScore[0].innerHTML=clientScore;
+  orcScore[0].innerHTML=comScore;
+  ketqua1= ketqua(clientScore,comScore);
+  endGameMess[0].innerHTML=ketqua1;
+  console.log(clientChoice);
+  console.log(comChoice);
+  console.log(message);
+  console.log('Điểm của bạn: ',clientScore);
+  console.log('Điểm của máy: ',comScore);
+});
 
-document.getElementsByClassName("paper").onclick = function() {youchoosepaper()};
-function youchooserock(){
-  return clientChoice='paper';
-}
+let paper = document.getElementsByClassName("paper");
+paper[0].addEventListener("click", function() {
+  clientChoice='paper';
+  let comNumber=getComNumber(1,3);
+  let comChoice=getComputerChoice(comNumber);
+  let message= playRound(comChoice,clientChoice);
+  ketqua1= ketqua(clientScore,comScore);
+  yourScore[0].innerHTML=clientScore;
+  orcScore[0].innerHTML=comScore;
+  endGameMess[0].innerHTML=ketqua1;
+  console.log('Điểm của bạn: ',clientScore);
+  console.log('Điểm của máy: ',comScore);
+  console.log(clientChoice);
+  console.log(comChoice);
+  console.log(message);
+});
 
-document.getElementsByClassName("scis").onclick = function() {youchoosescis()};
-function youchooserock(){
-  return clientChoice='scissor';
-}
-console.log(clientChoice);
-      // let comNumber=getComNumber(1,3);
-      // let comChoice=getComputerChoice(comNumber);
-      // let message= playRound(comChoice,clientChoice);
-      // console.log(clientChoice);
-      // console.log(comChoice);
-      // console.log(message);
-    // game(5);
-    // let ketqua;
-    // if(clientScore>comScore){
-    //   ketqua='Bạn đã chiến thắng chung cuộc!!!!!!!!';
-    // } else if(clientScore==comScore){
-    //   ketqua='Hai bạn đã hòa nhau.';
-    // } else {
-    //   ketqua='Bạn đã thất bại hoàn toàn :)))))))';
-    // }
-    //     console.log(ketqua);
-    //     console.log('Điểm của bạn: ',clientScore);
-    //     console.log('Điểm của máy: ',comScore);
+let scis = document.getElementsByClassName("scis");
+scis[0].addEventListener("click", function() {
+  clientChoice='scissor';
+  let comNumber=getComNumber(1,3);
+  let comChoice=getComputerChoice(comNumber);
+  let message= playRound(comChoice,clientChoice);
+  ketqua1= ketqua(clientScore,comScore);
+  yourScore[0].innerHTML=clientScore;
+  orcScore[0].innerHTML=comScore;
+  endGameMess[0].innerHTML=ketqua1;
+  console.log('Điểm của bạn: ',clientScore);
+  console.log('Điểm của máy: ',comScore);
+  console.log(clientChoice);
+  console.log(comChoice);
+  console.log(message);
+});
+
